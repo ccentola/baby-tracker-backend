8d0764a963a156688fe522b70054d04cf26883bb
Carl Centola <carl.centola@gmail.com>
2023-08-29 06:01:47 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# baby-tracker-backend
+
+Backend for baby activity tracking application built on Node.js.
---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const { logger } = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
+
+const PORT = process.env.PORT || 3500;
+
+/*
+================================================================================
+MIDDLEWARE
+================================================================================
+*/
+
+// logger
+app.use(logger);
+
+// error handler
+app.use(errorHandler);
+
+/*
+================================================================================
+ROUTE REGISTRATION
+================================================================================
+*/
+
+app.use('/', require('./routes/root'));
+
+// catchall
+app.all('*', (req, res) => {
+  res.status(404);
+  if (req.accepts('html')) {
+    res.sendFile(path.join(__dirname, 'views', '404.html'));
+  }
+  if (req.accepts('json')) {
+    res.json({ error: '404 Not Found' });
+  } else {
+    res.type('txt').send('404 Not Found');
+  }
+});
+
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
